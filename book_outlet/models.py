09f2464d221ae12d3aede4d286e6398a6493dc86
Motoki Higa <mokkyhiga@gmail.com>
2022-10-20 22:39:56 +0900

    feat: add routes for list and detail page. Also pass the data through context

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 # https://docs.djangoproject.com/en/4.1/ref/models/fields/
+
+
 class Book(models.Model):
-  title = models.CharField(max_length=50)
-  rating = models.IntegerField()
+    title = models.CharField(max_length=50)
+    ratings = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    # Below is to customise how the data is displayed in <QuerySet> in console
+
+    def __str__(self):
+        return f'{self.title} ({self.ratings})'
